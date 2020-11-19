22d1259bfc06664b4c92f77adddd56bfed64dd7a
LongKiny <68689675+LongKiny@users.noreply.github.com>
2020-11-19 16:22:27 +0700

    Demo commiit github

---
+++ selenium/webdriver_api/Topic_01_Check_Environment.java
@@ -0,0 +1,65 @@
+package webdriver_api;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Topic_01_Check_Environment {
+    WebDriver driver;
+
+    @BeforeClass
+    public void beforeClass()  {
+        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/browserDrivers/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.get("http://demo.guru99.com/v4/");
+    }
+
+    @Test
+    public void TC_01_ValidateCurrentUrl() {
+        String loginPageUrl = driver.getCurrentUrl();
+        Assert.assertEquals(loginPageUrl, "http://demo.guru99.com/v4/");
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void TC_02_ValidatePageTitle() {
+        String loginPageTitle = driver.getTitle();
+        Assert.assertEquals(loginPageTitle, "Guru99 Bank Home Page");
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void TC_03_LoginFormDisplayed() {
+        Assert.assertTrue(driver.findElement(By.xpath("//form[@name='frmLogin']")).isDisplayed());
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @AfterClass
+    public void afterClass() {
+        driver.quit();
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
